59ea15a9662e3bd860288c9037b2e064a40849dd
Han Seok Yoon <zergyoon2@icloud.com>
2022-09-23 12:11:08 +0900

    220923_1211

--- functional.js
+++ functional.js
@@ -1,23 +1,32 @@
 
+
+
 $(document).ready(function(){
 
 // ===== API NO.1 =====
-  var nowDate = Date(); // yyyy-mm-dd로 아직 변환 못해서 임의로 넣어둠
-  console.log(nowDate);
+  var dateData = new Date(); // yyyy-mm-dd로 아직 변환 못해서 임의로 넣어둠
+  var nowDate = `${dateData.getFullYear()}-0${dateData.getMonth()+1}-${dateData.getDate()}`;
+  // var today = Temporal.Now.plainDateISO();
 
+  console.log(nowDate);
+  // console.log(today.toString());
+
+  var api1 =
   $.ajax({
     url: "https://Ilruwain.com/api/testApi1",
     type: "get",
-    data: {date: "2022-09-22", user: "test1"}, //임의로 날짜 넣어봄
-    dataType: "JSON",
+    data: {date: nowDate, user: "test1"}, //임의로 날짜 넣어봄
+    // dataType: "JSON",
     success: function(data) {
       var api1Hash = JSON.parse(data.test_data).hash;
       // test_data 값의 문자열 타입을 객체로 바꾸고, hash 값 추출
       var api1Hash2 = JSON.parse(data.test_data).hash2;
       // test_data 값의 문자열 타입을 객체로 바꾸고, hash2 값 추출
 
-      // console.log(data);
-      // console.log(data.test_data);
+      //지역변수로 만든 api1Hash를 리턴해서 변수api1에 담아볼까 했는데, 나 작동하지 않음
+
+      // return JSON.stringify(api1Hash);
+
       console.log("Hash 값: "+api1Hash);
       console.log("Hash2 값: "+api1Hash2);
     },
@@ -32,57 +41,64 @@ $(document).ready(function(){
 
 // ===== API NO.1 END =====
 
+console.log(api1);
+// 현 상태에서 api를 출력하면 [object] [object]로 출력 => 오브젝트상태라는 뜻
+
+var api1Hash = JSON.stringify(api1);
+console.log(api1Hash);
+
+
 // ===== API NO.2 =====
-  $.ajax({
-    url: "https://Ilruwain.com/api/testApi2",
-    type: "post",
-    data: {hash1: api1Hash, hash2: api1Hash2},
-    dataType: "JSON",
-    async: false,
-    success: function(data) {
-      var api3hash3 = JSON.parse(data.hashed_data).hash3;
-      // test_data 값의 문자열 타입을 객체로 바꾸고, hash 값 추출
-
-      var api3hash4 = JSON.parse(data.hashed_data).hash4;
-      // test_data 값의 문자열 타입을 객체로 바꾸고, hash 값 추출
-
-      $(".diaryWritten").append(api3hash4);
-      console.log(api3hash4);
-    },
-    error: function(data) {
-      if(data.status === 500) {
-        alert("서버에 오류가 발생했습니다");
-      } else {
-        alert(data.message + data.code);
-      }
-    },
-  });
+  // $.ajax({
+  //   url: "https://Ilruwain.com/api/testApi2",
+  //   type: "post",
+  //   data: {hash1: api1Hash, hash2: api1Hash2},
+  //   dataType: "JSON",
+  //   // async: false,
+  //   success: function(data) {
+  //     var api3hash3 = JSON.parse(data.hashed_data).hash3;
+  //     // test_data 값의 문자열 타입을 객체로 바꾸고, hash 값 추출
+  //
+  //     var api3hash4 = JSON.parse(data.hashed_data).hash4;
+  //     // test_data 값의 문자열 타입을 객체로 바꾸고, hash 값 추출
+  //
+  //     $(".diaryWritten").append(api3hash4);
+  //     console.log(api3hash4);
+  //   },
+  //   error: function(data) {
+  //     if(data.status === 500) {
+  //       alert("서버에 오류가 발생했습니다");
+  //     } else {
+  //       alert(data.message + data.code);
+  //     }
+  //   },
+  // });
 // ===== API NO.2 END =====
 
 
 // ===== API NO.3 =====
-  $.ajax({
-    url: "https://Ilruwain.com/api/testApi3",
-    type: "post",
-    data: {hash1: api1Hash, type: "text"},
-    dataType: "JSON",
-    success: function(data) {
-      var api3Name = data.name;
-      var api3Text = data.text;
-      var api3Sentence = data.name + data.text;
-
-      $(".diaryWritten").append(api3Name+api3Text);
-      console.log(api3Name+api3Text);
-      console.log(api3Sentence);
-    },
-    error: function(data) {
-      if(data.status === 500) {
-        alert("서버에 오류가 발생했습니다");
-      } else {
-        alert(data.message + data.code);
-      }
-    },
-  });
+  // $.ajax({
+  //   url: "https://Ilruwain.com/api/testApi3",
+  //   type: "post",
+  //   data: {hash1: api1Hash, type: "text"},
+  //   dataType: "JSON",
+  //   success: function(data) {
+  //     var api3Name = data.name;
+  //     var api3Text = data.text;
+  //     var api3Sentence = data.name + data.text;
+  //
+  //     $(".diaryWritten").append(api3Name+api3Text);
+  //     console.log(api3Name+api3Text);
+  //     console.log(api3Sentence);
+  //   },
+  //   error: function(data) {
+  //     if(data.status === 500) {
+  //       alert("서버에 오류가 발생했습니다");
+  //     } else {
+  //       alert(data.message + data.code);
+  //     }
+  //   },
+  // });
 // ===== API NO.3 END =====
 
 
