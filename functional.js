28548060e93a77db4950377cad947d4def66f64a
Han Seok Yoon <zergyoon2@icloud.com>
2022-09-26 13:56:11 +0900

    220926_1356

--- functional.js
+++ functional.js
@@ -6,50 +6,82 @@ $(document).ready(function(){
   var dateData = new Date();
   var nowDate = `${dateData.getFullYear()}-0${dateData.getMonth()+1}-${dateData.getDate()}`;
 
-  var api1Hash;
-  var api1Hash2;
-  var api1Hash3;
-  var api1Hash4;
-  var api1Hash5;
-  var name;
-  var text;
-
 // ===== API 1 START =====
   $.ajax({
     url: "https://Ilruwain.com/api/testApi1",
     type: "get",
     data: {date: nowDate, user: "test1"},
     success: function(data) {
-      api1Hash = JSON.parse(data.test_data).hash;
-      api1Hash2 = JSON.parse(data.test_data).hash2;
+      var api1Hash = JSON.parse(data.test_data).hash;
+      var api1Hash2 = JSON.parse(data.test_data).hash2;
       console.log("API1_hash= "+api1Hash);
       console.log("API1_hash2= "+api1Hash2);
 
-      // ===== API 3 START =====
+      // ===== API 2 START =====
       $.ajax({
-        url: "https://Ilruwain.com/api/testApi3",
+        url: "https://Ilruwain.com/api/testApi2",
         type: "post",
-        data: {hash1: api1Hash, type: "text"},
+        data: {hash1: api1Hash, hash2: api1Hash2},
         success: function(data) {
-          name = data.name;
-          text = data.text;
-          $(".diaryWritten").empty();
-          $(".diaryWritten").append(name+" ");
-          $(".diaryWritten").append(text+" ");
-          console.log("name= "+name);
-          console.log("text= "+text);
+          var api2Hash3 = JSON.parse(data.hashed_data).hash3;
+          var api2Hash4 = JSON.parse(data.hashed_data).hash4;
+          $(".diaryWritten").append(api2Hash4+" 줬다.");
+          console.log("API2_hash3= "+api2Hash3);
+          console.log("API2_hash4= "+api2Hash4);
 
-          // ===== API 2 START =====
+          // ===== API 3 START =====
           $.ajax({
-            url: "https://Ilruwain.com/api/testApi2",
+            url: "https://Ilruwain.com/api/testApi3",
             type: "post",
-            data: {hash1: api1Hash, hash2: api1Hash2},
+            data: {hash1: api1Hash, type: "text"},
             success: function(data) {
-              api1Hash3 = JSON.parse(data.hashed_data).hash3;
-              api1Hash4 = JSON.parse(data.hashed_data).hash4;
-              $(".diaryWritten").append(api1Hash4+" 줬다.");
-              console.log("API2_hash3= "+api1Hash3);
-              console.log("API2_hash4= "+api1Hash4);
+              var name = data.name;
+              var text = data.text;
+              $(".diaryWritten").empty();
+              $(".diaryWritten").append(name+" ");
+              $(".diaryWritten").append(text+" ");
+              console.log("API3_name= "+name);
+              console.log("API3_text= "+text);
+              
+              // ===== API 4 START =====
+              $.ajax({
+                url: "https://Ilruwain.com/api/testApi4",
+                type: "get",
+                data: {user: "test2", hash1: api1Hash},
+                success: function(data) {
+                  var api1Hash5 = data.hash5;
+                  console.log("API4_hash5= "+api1Hash5);
+
+                  // ===== API 5 START =====
+                  $.ajax({
+                    url: "https://Ilruwain.com/api/testApi5",
+                    type: "post",
+                    data: {user: "test2", hash5: api1Hash5},
+                    success: function(data) {
+                      var title = data.title;
+                      $(".diaryTitle").append(title);
+                      $(".diaryWritten").append("로딩중...");
+                      console.log("API5_title= "+title);
+                    },
+                    error: function(data) {
+                      if(data.status === 500) {
+                        alert("서버에 오류가 발생했습니다");
+                      } else {
+                        alert(data.message + data.code);
+                      }
+                    },
+                  });
+                  // ===== API 5 END =====
+                },
+                error: function(data) {
+                  if(data.status === 500) {
+                    alert("서버에 오류가 발생했습니다");
+                  } else {
+                    alert(data.message + data.code);
+                  }
+                },
+              });
+              // ===== API 4 END =====
             },
             error: function(data) {
               if(data.status === 500) {
@@ -59,7 +91,7 @@ $(document).ready(function(){
               }
             },
           });
-          // ===== API 2 END =====
+          // ===== API 3 END =====
         },
         error: function(data) {
           if(data.status === 500) {
@@ -69,47 +101,9 @@ $(document).ready(function(){
           }
         },
       });
-      // ===== API 3 END =====
+      // ===== API 2 END =====
 
-      // ===== API 4 START =====
-      $.ajax({
-        url: "https://Ilruwain.com/api/testApi4",
-        type: "get",
-        data: {user: "test2", hash1: api1Hash},
-        success: function(data) {
-          api1Hash5 = data.hash5;
-          console.log("API2_hash5= "+api1Hash5);
 
-          // ===== API 5 START =====
-          $.ajax({
-            url: "https://Ilruwain.com/api/testApi5",
-            type: "post",
-            data: {user: "test2", hash5: api1Hash5},
-            success: function(data) {
-              title = data.title;
-              $(".diaryTitle").append(title);
-              $(".diaryWritten").append("로딩중...");
-              console.log("title= "+title);
-            },
-            error: function(data) {
-              if(data.status === 500) {
-                alert("서버에 오류가 발생했습니다");
-              } else {
-                alert(data.message + data.code);
-              }
-            },
-          });
-          // ===== API 5 END =====
-        },
-        error: function(data) {
-          if(data.status === 500) {
-            alert("서버에 오류가 발생했습니다");
-          } else {
-            alert(data.message + data.code);
-          }
-        },
-      });
-      // ===== API 4 END =====
 
     },
     error: function(data) {
